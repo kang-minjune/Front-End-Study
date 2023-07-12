324df7143a4fbefd957e2f2f0e56531ede3da2cb
Kang-Min Jun <100847512+kang-minjune@users.noreply.github.com>
2023-07-12 19:59:57 +0900

    학술제 졸업조건 계산 웹 개발코드 (재업로드 : 2023.07.12)
    개발 환경: Visual Studio Code
    개발 언어: HTML/CSS/JS

---
+++ [학술제]졸업조건 계산 웹/JS/index.js
@@ -0,0 +1,25 @@
+var control = document.getElementById('control');
+var bar = document.querySelector('.bar');
+var value = document.querySelector('.value');
+
+var RADIUS = 54;
+var CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+
+function progress(per) {
+  var progress = per / 100;
+  var dashoffset = CIRCUMFERENCE * (1 - progress);
+  
+  value.innerHTML=per +'%';
+  bar.style.strokeDashoffset = dashoffset;
+}
+
+control.addEventListener('input', function(event) {
+  progress(event.target.valueAsNumber);
+});
+control.addEventListener('change', function(event){
+  progress(event.target.valueAsNumber);
+});
+
+bar.style.strokeDasharray = CIRCUMFERENCE;
+progress(60);
+
